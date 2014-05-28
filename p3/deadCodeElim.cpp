9f40d5555b3ee1a3f06089834873a137e87884af
mfbassora <mfbassora@gmail.com>
2014-05-28 16:16:52 -0300

    asd

--- p3/deadCodeElim.cpp
+++ p3/deadCodeElim.cpp
@@ -26,53 +26,16 @@ namespace {
          for (BasicBlock::iterator i2 = i->begin(), e2 = i->end(); i2 != e2; )
          {
             
-             if (LoadInst* loadInst = dyn_cast<LoadInst>(&*i2)) {
-                //Caso a instrucao seja de load, vamos verificar se a anterior e uma instrucao de store
-                  if (StoreInst* storeInst = dyn_cast<StoreInst>(&*last_inst)) 
-                  {
-                      
-//                      errs() << "Load na variavel"<< loadInst->getOperand(0)<<" \n";
-//                      errs() << "Store na variavel"<< storeInst->getOperand(1)<<" \n";
-                      if(loadInst->getOperand(0)->getName()==storeInst->getOperand(1)->getName())
-                      {
-                          //Temos aqui um Load a ser tirado do codigo
-                          errs() << "Load inutil na variavel : "<< loadInst->getOperand(0)->getName() <<" \n";
-                          //Vamos dar um replace no registrador da funcao load
-                         loadInst->replaceAllUsesWith(storeInst->getValueOperand());
-                          //Removemos entao o load
-                          //errs() << "Deletando a funcao de load  \n ";
-                          loadRemove=loadInst;
-                         //Vai entrar na parte da remocao
-                          remove =1;
-                          ret=true;
-                          
-                      }
-             
-                  };
-             
-//                   errs() << last_inst->getName() << "\n";
-//                 errs() << "Temos uma funcao de load" << callInst->getOperand(0)->getName() << "\n";
-//                
-          }
-             last_inst = i2;
-             i2++;
-             //Removendo
-           if(remove ==1)
-             {
-               errs() << "Deletando a funcao de load " << loadRemove->getOperand(0)->getName()<< " \n ";
-               loadRemove->eraseFromParent();
-                 remove =0;
-                 
-             }
-             
             
              
+          }
+             
         }
-        }
+        
             
        
           
-      return ret;
+      return true;
     }
   };
 }
